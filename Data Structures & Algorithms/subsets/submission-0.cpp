19a88663faac27cd82fb2ddb700eb68433a9b294
smalltsa <92347535+smalltsa@users.noreply.github.com>
2026-08-29 19:14:43 +0800

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void dfs(vector<int>& nums,int i,vector<int> & cur){
+        if (i==nums.size()){
+            ans.push_back(cur);
+            return;
+        }
+        cur.push_back(nums[i]);
+        dfs(nums,i+1,cur);
+        cur.pop_back();
+        dfs(nums,i+1,cur);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> cur;
+        dfs(nums,0,cur);
+        return ans;
+    }
+};
